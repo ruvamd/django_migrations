395ebb62a01c5486de6dae0545df978af6922ba2
vadim rusu <ruvamd@gmail.com>
2023-11-14 15:11:50 -0800

    done

--- historical_data/models.py
+++ historical_data/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class PriceHistory(models.Model):
     date=models.DateTimeField(auto_now_add=True)
     price=models.DecimalField(max_digits=7,decimal_places=2)
-    volume=models.PositiveIntegerField()
+    volume=models.DecimalField(max_digits=7,decimal_places=3)
     
